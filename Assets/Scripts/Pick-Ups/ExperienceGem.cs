6d6dc434d6add16d68fac8b4983449202a16e09e
SaladWaster <767asess@gmail.com>
2023-09-10 16:23:03 +0800

    TextMashPro Update
    Updated Scripts and Text components to TextMashPro

--- Assets/Scripts/Pick-Ups/ExperienceGem.cs
+++ Assets/Scripts/Pick-Ups/ExperienceGem.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExperienceGem : PickUp, ICollectable
+public class ExperienceGem : PickUp
 {
     public int experienceGranted;
 
-    public void Collect()
+    public override void Collect()
     {
+        if(hasBeenCollected)
+        {
+            return;
+        }
+        else
+        {
+            // Executes the base implementation of the Collect method from PickUp class
+            base.Collect();
+        }
+
         // Reference the PlayerStats script
         PlayerStats player = FindObjectOfType<PlayerStats>();
         player.IncreaseExperience(experienceGranted);
